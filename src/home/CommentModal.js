a94a22a6ce67c0486d673239c856b876035200a1
HaesungKang <koh1260@naver.com>
2023-02-02 14:45:31 +0900

    CommentModal 컴포넌트 구성 틀잡기

--- src/home/CommentModal.js
+++ src/home/CommentModal.js
@@ -1,24 +1,27 @@
 import styles from "./CommentModal.module.css";
 
-function CommentModal({ setOpenCommentModal }) {
+function CommentModal({ setOpenCommentModal, image }) {
+  console.log(image);
   function closeCommentModal() {
     setOpenCommentModal(false);
   }
 
   return (
     <div className={styles.main}>
-      <div>
+      <div className={styles.comment_modal}>
         <button onClick={closeCommentModal} className={styles.close_btn}>
           X
         </button>
 
-        <div className={styles.contents}>
-          <div>
-            <img />
+        <div className={styles.content}>
+          <div className={styles.image_container}>
+            <div className={styles.image_outer}>
+              <img src={image} />
+            </div>
           </div>
-          <div>
+          <div className={styles.profile_and_comment_container}>
             {/* 프로필 */}
-            <div>
+            <div className={styles.profile}>
               <div>
                 <img />
               </div>
@@ -26,12 +29,21 @@ function CommentModal({ setOpenCommentModal }) {
               <div>...</div>
             </div>
             {/* 댓글 */}
-            <div></div>
+            <div className={styles.comment}>댓글댓글</div>
             {/* 좋아요 ~ 댓글 달기 */}
             <div>
-              <div></div>
-              <div></div>
-              <div></div>
+              <div className={styles.utils}>
+                <div>좋아요</div>
+                <div>댓글</div>
+                <div>공유</div>
+                <div>북마크</div>
+              </div>
+              <div className={styles.likes_count}>좋아요 수</div>
+              <div className={styles.create_at}>게시 날</div>
+              <div className={styles.writing_comment}>
+                <div>스마일</div>
+                <input />
+              </div>
             </div>
           </div>
         </div>
--- src/home/Post.js
+++ src/home/Post.js
@@ -1,92 +1,96 @@
 import { useState } from "react";
 import styles from "./Post.module.css";
 import img from "../img/profile.jpeg";
-import {BiDotsHorizontalRounded} from "react-icons/bi";
-import {AiOutlineHeart} from "react-icons/ai";
-import {HiOutlineChatBubbleOvalLeft} from "react-icons/hi2"
-import {IoPaperPlaneOutline} from "react-icons/io5";
-import {BsBookmark} from "react-icons/bs"
-import {VscSmiley} from "react-icons/vsc"
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { AiOutlineHeart } from "react-icons/ai";
+import { HiOutlineChatBubbleOvalLeft } from "react-icons/hi2";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { BsBookmark } from "react-icons/bs";
+import { VscSmiley } from "react-icons/vsc";
 import CommentModal from "./CommentModal";
 
-function Post({userId, image, content, likes, createAt, comment}){
-    const [openCommentModal, setOpenCommentModal] = useState(false);
+function Post({ userId, image, content, likes, createAt, comment }) {
+  const [openCommentModal, setOpenCommentModal] = useState(false);
 
-    const postTimeCalc = (time) => {
-      if (time >= 3600) return `${Math.floor(time / 3600)}시간 전`;
-      if (time >= 60) return `${Math.floor(time / 60)}분 전`;
-      return `${time}초 전`;
-    }
+  const postTimeCalc = (time) => {
+    if (time >= 3600) return `${Math.floor(time / 3600)}시간 전`;
+    if (time >= 60) return `${Math.floor(time / 60)}분 전`;
+    return `${time}초 전`;
+  };
 
-    function openAllComment(){
-      setOpenCommentModal(true);
-      console.log(openCommentModal);
-    }
+  function openAllComment() {
+    setOpenCommentModal(true);
+  }
 
-    return (
-      <div className={styles.main}>
-        <div className={styles.post}>
-          <div className={styles.profile}>
-            <div className={styles.profile_image_outer}>
-              <img className={styles.profile_image} src={img} />
-            </div>
-            <div className={styles.user_id}>{userId}</div>
-            <div>
-              <BiDotsHorizontalRounded />
-            </div>
+  return (
+    <div className={styles.main}>
+      <div className={styles.post}>
+        <div className={styles.profile}>
+          <div className={styles.profile_image_outer}>
+            <img className={styles.profile_image} src={img} />
           </div>
-
-          <div className={styles.contents}>
-            <img className={styles.content_image} src={image} />
-          </div>
-
-          {/* 좋아요 ~ 게시 시간 */}
-          <div className={styles.post_footer}>
-            <div className={styles.utils}>
-              <div className={styles.util_outer}>
-                <AiOutlineHeart className={styles.util} />
-              </div>
-
-              <div className={styles.util_outer}>
-                <HiOutlineChatBubbleOvalLeft 
-                  onClick={openAllComment}
-                  className={styles.util} />
-                  {openCommentModal ? <CommentModal setOpenCommentModal={setOpenCommentModal}/> : null}
-              </div>
-
-              <div className={styles.util_outer}>
-                <IoPaperPlaneOutline className={styles.util} />
-              </div>
-
-              <div className={styles.book_mark_outer}>
-                <BsBookmark className={`${styles.util} ${styles.book_mark}`} />
-              </div>
-            </div>
-
-            <div className={styles.likes_count}>{`좋아요 ${likes}개`}</div>
-            <div className={styles.post_user_and_content}>
-              <span className={styles.post_user}>{userId}</span>
-              <span className={styles.content}>{content}</span>
-            </div>
-            {comment.length ? (
-              <div
-                className={styles.comments_count}
-              >{`댓글 ${comment.length}개 모두 보기`}</div>
-            ) : null}
-            <div className={styles.create_at}>{postTimeCalc(createAt)}</div>
-          </div>
-
-          {/* 댓글 작성 */}
-          <div className={styles.comment_writing}>
-            <div className={styles.comment_icon_outer}>
-              <VscSmiley className={styles.comment_icon}/>
-            </div>
-            <input 
-              className={styles.comment_input}
-              placeholder="댓글 달기..."/>
+          <div className={styles.user_id}>{userId}</div>
+          <div>
+            <BiDotsHorizontalRounded />
           </div>
         </div>
+
+        <div className={styles.contents}>
+          <img className={styles.content_image} src={image} />
+        </div>
+
+        {/* 좋아요 ~ 게시 시간 */}
+        <div className={styles.post_footer}>
+          <div className={styles.utils}>
+            <div className={styles.util_outer}>
+              <AiOutlineHeart className={styles.util} />
+            </div>
+
+            {/* 댓글 버튼 */}
+            <div className={styles.util_outer}>
+              <HiOutlineChatBubbleOvalLeft
+                onClick={openAllComment}
+                className={styles.util}
+              />
+              {openCommentModal ? (
+                <CommentModal
+                  setOpenCommentModal={setOpenCommentModal}
+                  image={image}
+                />
+              ) : null}
+            </div>
+
+            <div className={styles.util_outer}>
+              <IoPaperPlaneOutline className={styles.util} />
+            </div>
+
+            <div className={styles.book_mark_outer}>
+              <BsBookmark className={`${styles.util} ${styles.book_mark}`} />
+            </div>
+          </div>
+
+          <div className={styles.likes_count}>{`좋아요 ${likes}개`}</div>
+          <div className={styles.post_user_and_content}>
+            <span className={styles.post_user}>{userId}</span>
+            <span className={styles.content}>{content}</span>
+          </div>
+          {comment.length ? (
+            <div
+              className={styles.comments_count}
+            >{`댓글 ${comment.length}개 모두 보기`}</div>
+          ) : null}
+          <div className={styles.create_at}>{postTimeCalc(createAt)}</div>
+        </div>
+
+        {/* 댓글 작성 */}
+        <div className={styles.comment_writing}>
+          <div className={styles.comment_icon_outer}>
+            <VscSmiley className={styles.comment_icon} />
+          </div>
+          <input className={styles.comment_input} placeholder="댓글 달기..." />
+        </div>
       </div>
-    );   
+    </div>
+  );
 }
 export default Post;
