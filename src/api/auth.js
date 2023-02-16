7ad79a54c93d41b53779361bf3ffed4280aca98d
HaesungKang <koh1260@naver.com>
2023-02-17 06:11:50 +0900

    Feat: 회원 정보 조회 api 호출 추가

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,16 +3,22 @@ import http from './http';
 
 class AuthService{
     get(){
-        return http.get('/user');
+        return http.get('/auth/user');
     }
     login(data){
-        return http.post('/login', data);
+        return http.post('/auth/login', data);
     }
     logout(){
-        return http.get('/logout');
+        return http.get('/auth/logout');
     }
     signup(data){
-        return http.post('/sign_up', data);
+        return http.post('/auth/sign-up', data);
+    }
+    loginedUser(){
+        return http.get('/auth/logined-user');
+    }
+    user(userId){
+        return http.get(`/auth/user/${userId}`);
     }
 }
 
