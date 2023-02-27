ddc444ff1b36052a7b9ccd595094c4a60b04eec0
HaesungKang <koh1260@naver.com>
2023-02-28 04:42:47 +0900

    Feat: profile 컴포넌트 user 정보 요청 추가
    sesisonStorage에 저장힌 nickname으로 user 정보 서버에 요청

--- src/routes/ProfilePage.js
+++ src/routes/ProfilePage.js
@@ -5,10 +5,14 @@ import MenuBar from "../components/ProfilePage/MenuBar";
 import ProfilePost from "../components/ProfilePage/ProfilePost";
 import { useEffect, useState } from "react";
 import PostService from "../api/post";
+import AuthService from '../api/auth';
+import { useParams } from "react-router-dom";
 
 export default function ProfilePage() {
+  const { nickname } = useParams();
   const [posts, setPosts] = useState([]);
-
+  const [user, setUser] = useState();
+  console.log(nickname);
   useEffect(() => {
     async function getPosts() {
       const response = await PostService.loginedPosts();
@@ -16,13 +20,20 @@ export default function ProfilePage() {
       const postsList = response.data;
       setPosts(postsList);
     }
+    async function getUserInfo(){
+      const response = await AuthService.userByNickname(nickname);
+      console.log('userInfoByNickname: ',response);
+      const user = response.data;
+      setUser(user);
+    }
+    getUserInfo();
     getPosts();
   }, []);
 
   // function postsView(){
   //   for(let i =0; i < Math.ceil(posts.length / 3); i++){
   //     <div>
-        
+
   //     </div>
   //   }
   // }
@@ -30,23 +41,33 @@ export default function ProfilePage() {
   return (
     <div>
       <SideBar />
+      {user && posts ? (
       <div className={styles.contents}>
-        <Profile />
-        <MenuBar />
-        <div className={styles.contents_post}>
-            {posts ? (posts.map((post) => 
-                <ProfilePost   
-                key={post.postId}
-                postId={post.postId} 
-                content={post.content}
-                createdAt={post.createdAt}
-                image={post.Images}
-                user={post.User}
-                comments={post.Comments}
-                />
-            )) : <div>앖졍</div>}
-        </div>
+      <Profile 
+        user={user}
+        postsLength={posts.length}
+        />
+      <MenuBar />
+      <div className={styles.contents_post}>
+        {posts ? (
+          posts.map((post) => (
+            <ProfilePost
+              key={post.postId}
+              postId={post.postId}
+              content={post.content}
+              createdAt={post.createdAt}
+              image={post.Images}
+              user={post.User}
+              comments={post.Comments}
+            />
+          ))
+        ) : (
+          <div>앖졍</div>
+        )}
       </div>
+    </div>
+      ) : <div>Loading. . .</div>}
+
     </div>
   );
 }
