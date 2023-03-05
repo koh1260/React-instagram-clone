eb64355cd791fb935de7fa7fe9a68636247131b9
HaesungKang <koh1260@naver.com>
2023-03-06 06:17:20 +0900

    Refactor: Article  컴포넌트 제거, LoginPage 컴포넌트에 병합

--- src/components/SignInUp/page/LoginPage.js
+++ src/components/SignInUp/page/LoginPage.js
@@ -1,17 +1,34 @@
-import Article from "../template/Article";
 import Footer from "../organisms/Footer";
-import styles from "./LoginPage.module.css"
+import styles from "./LoginPage.module.css";
+import Login from "../organisms/Login";
+import SignUp from "../organisms/SignUp";
+import Animation from "../atoms/Animation";
+import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
 
-function LoginPage(){
-    return (
-      <div className={styles.main}>
-        <div className={styles.article}>
-          <Article />
-        </div>
-        <div className={styles.footer}>
-          <Footer />
+function LoginPage() {
+  const responsivePage = useMediaQuery({ query: "(min-width: 875px)" });
+  
+  return (
+    <div className={styles.main}>
+      <div className={styles.article}>
+        <div className={styles.article_body}>
+          <div className={styles.main_a}>
+            {responsivePage && (
+              <div>
+                <Animation />
+              </div>
+            )}
+            <div>
+              <Login />
+            </div>
+          </div>
         </div>
       </div>
-    );
+      <div className={styles.footer}>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 export default LoginPage;
