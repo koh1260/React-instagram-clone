5f9a8d9b4a9babdb85d118aa1d9018df57b3f86a
HaesungKang <koh1260@naver.com>
2023-03-06 22:07:19 +0900

    rebase

--- src/components/SignInUp/molecules/LoginForm.js
+++ src/components/SignInUp/molecules/LoginForm.js
@@ -4,32 +4,14 @@ import SignInUpButton from "../atoms/SignInUpButton";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-<<<<<<< HEAD
 import AuthService from "../../../api/auth";
 import loginSlice from "../../../redux/slice/loginSlice";
 //placeholder, onChange, value
 function LoginForm() {
-=======
-import loginSlice from "../../../redux/login/loginSlice";
-//placeholder, onChange, value
-function LoginForm({
-  onSubmit,
-  inputType01,
-  inputType02,
-  placeholder01,
-  onChange01,
-  value01,
-  placeholder02,
-  onChange02,
-  value02,
-  btnText,
-}) {
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
   const navigate = useNavigate();
   const dipatch = useDispatch();
   const email = useSelector((state) => state.login.email);
   const password = useSelector((state) => state.login.password);
-<<<<<<< HEAD
   const sessionStorage = window.sessionStorage;
 
   async function loginSubmit(e) {
@@ -48,45 +30,6 @@ function LoginForm({
     } else {
       alert("로그인 실패");
     }
-=======
-  const myStorage = window.sessionStorage;
-  console.log("email: ", email);
-  console.log("password: ", password);
-
-  function loginSubmit(e) {
-    e.preventDefault();
-    const data = {
-      email: email,
-      password: password,
-    };
-    const config = {
-      method: "post",
-      url: "http://localhost:4000/auth/login",
-      data: data,
-      withCredentials: true,
-    };
-
-    axios(config)
-      .then((response) => {
-        if (response.status === 200) {
-          console.log("session storege에 저장할 값: ", response.data.nickname);
-          myStorage.setItem("nickname", response.data.nickname);
-          navigate("/home");
-        }
-      })
-      .catch((err) => console.log(err));
-  }
-  // dispatch를 하는데 하나만 하니까 다른 하나의 값은 undifined로 업데이트되는 거 같다.
-  function handleInputEmail(e) {
-    dipatch(
-      loginSlice.actions.login({ email: e.target.value, password: password })
-    );
-  }
-  function handleInputPassword(e) {
-    dipatch(
-      loginSlice.actions.login({ email: email, password: e.target.value })
-    );
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
   }
 
   return (
@@ -94,23 +37,16 @@ function LoginForm({
       <form className={styles.login_form} onSubmit={(e) => loginSubmit(e)}>
         <div className={styles.input_outer}>
           <SignInUpInput
-<<<<<<< HEAD
             inputType={"text"}
             placeholder={"전화번호, 사용자 이름 또는 이메일"}
             onChange={(e) =>
               dipatch(loginSlice.actions.inputEmail({ email: e.target.value }))
             }
-=======
-            inputType={inputType01}
-            placeholder={placeholder01}
-            onChange={handleInputEmail}
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
             value={email}
           />
         </div>
         <div className={styles.input_outer}>
           <SignInUpInput
-<<<<<<< HEAD
             inputType={"password"}
             placeholder={"비밀번호"}
             onChange={(e) =>
@@ -118,11 +54,6 @@ function LoginForm({
                 loginSlice.actions.inputPassword({ password: e.target.value })
               )
             }
-=======
-            inputType={inputType02}
-            placeholder={placeholder02}
-            onChange={handleInputPassword}
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
             value={password}
           />
         </div>
--- src/components/SignInUp/molecules/SignUpForm.js
+++ src/components/SignInUp/molecules/SignUpForm.js
@@ -3,7 +3,6 @@ import styles from "./SignUpForm.module.css";
 import SignInUpButton from "../atoms/SignInUpButton";
 import SignInUpInput from "../atoms/SignInUpInput";
 import axios from "axios";
-<<<<<<< HEAD
 import { useDispatch, useSelector } from "react-redux";
 import signUpSlice from "../../../redux/slice/signUpSlice";
 import AuthService from '../../../api/auth';
@@ -33,124 +32,39 @@ function SignUpForm() {
     console.log(response);
    
     dispatch(signUpSlice.actions.clear());
-=======
-
-function SignUpForm() {
-  const [inputEmail, setInputEmail] = useState("");
-  const [inputName, setInputName] = useState("");
-  const [inputNickname, setInputNickname] = useState("");
-  const [inputPw, setInputPw] = useState("");
-
-  function handleInputEmail(event) {
-    setInputEmail(event.target.value);
-  }
-  function handleInputName(event) {
-    setInputName(event.target.value);
-  }
-  function handleInputNickname(event) {
-    setInputNickname(event.target.value);
-  }
-  function handleInputPw(event) {
-    setInputPw(event.target.value);
-  }
-  // function handleSighUpPage() {
-  //   setSignUpPage(false);
-  // }
-  async function signUpSubmit(event) {
-    event.preventDefault();
-
-    if (
-      (inputEmail.length &&
-      inputName.length &&
-      inputNickname.length &&
-      inputPw.length) === 0
-    ) {
-      alert("정보 입력해");
-      return;
-    }
-    console.log(inputEmail && inputName && inputNickname && inputPw);
-
-    const data = {
-      email: inputEmail,
-      name: inputName,
-      nickname: inputNickname,
-      password: inputPw,
-    };
-
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/auth/sign-up",
-        data
-      );
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-
-    setInputEmail("");
-    setInputName("");
-    setInputNickname("");
-    setInputPw("");
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
   }
 
   return (
     <div>
-<<<<<<< HEAD
       <form className={styles.sign_up_form} onSubmit={(e) => signUpSubmit(e)}>
-=======
-      <form className={styles.sign_up_form} onSubmit={signUpSubmit}>
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
         <div className={styles.sign_up_form_inner}>
           <div className={styles.input_outer}>
             <SignInUpInput
               placeholder={"휴대폰 번호 또는 이메일 주소"}
-<<<<<<< HEAD
               onChange={(e) => dispatch(signUpSlice.actions.inputEmail({email:e.target.value}))}
               value={email}
-=======
-              onChange={handleInputEmail}
-              value={inputEmail}
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
             />
           </div>
           <div className={styles.input_outer}>
             <SignInUpInput
               placeholder={"성명"}
-<<<<<<< HEAD
               onChange={(e) => dispatch(signUpSlice.actions.inputName({name:e.target.value}))}
               value={name}
-=======
-              onChange={handleInputName}
-              value={inputName}
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
             />
           </div>
           <div className={styles.input_outer}>
             <SignInUpInput
               placeholder={"사용자 이름"}
-<<<<<<< HEAD
               onChange={(e) => dispatch(signUpSlice.actions.inputNickname({nickname:e.target.value}))}
               value={nickname}
-=======
-              onChange={handleInputNickname}
-              value={inputNickname}
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
             />
           </div>
           <div className={styles.input_outer}>
             <SignInUpInput
-<<<<<<< HEAD
               inputType={"password"}
               placeholder={"비밀번호"}
               onChange={(e) => dispatch(signUpSlice.actions.inputpassword({password:e.target.value}))}
               value={password}
-=======
-              inputType={'password'}
-              placeholder={"비밀번호"}
-              onChange={handleInputPw}
-              value={inputPw}
->>>>>>> 933e8fa (Feat: 로그인 redux 적용)
             />
           </div>
           <div>
